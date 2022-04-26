9acbab05d2ff3bdfa22abaa57e16e61890ea6214
aaron <aaron.wolf@gmail.com>
2022-04-26 13:47:25 +0300

    blah

--- components/results.js
+++ components/results.js
@@ -1,8 +1,10 @@
 export default function Results(results) {
-  const randomResults = results.results.sort(() => Math.random() - 0.5);
+  const randomResults = results.results
+    .sort(() => Math.random() - 0.5)
+    .slice(0, 5);
   return (
     <>
-      <h1>Your results</h1>
+      <h1>You should try </h1>
       {randomResults.map((result) => (
         <div key={result.name}>
           {result.name}: {result.family}
